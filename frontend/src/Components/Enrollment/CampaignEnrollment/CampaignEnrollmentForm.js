1194cc43920cbd8c75fd58535c7759f471f7c91b
Madhumitha <hattimadhumitha@gmail.com>
2024-03-23 00:23:41 +0530

    Changes in backend&frontend

--- frontend/src/Components/Enrollment/CampaignEnrollment/CampaignEnrollmentForm.js
+++ frontend/src/Components/Enrollment/CampaignEnrollment/CampaignEnrollmentForm.js
@@ -20,14 +20,14 @@ export default function CampaignEnrollmentForm() {
         <div>
             <label htmlFor="age" className='mb-2 mx-2 form-label'>Campaign Title</label>
 
-            <input type="text" placeholder=" Title of Campaign" {...register(" Title of Campaign", {required: true})} />
+            <input type="text" placeholder=" Title of Campaign" {...register("campaignTitle", {required: true})} />
 
         </div>
         <div className='row mb-2'>
             <div className='col-xs-1 col-6 mb-2'>
             <label htmlFor="firstname" className='mb-2 mx-2 form-label'>Firstname</label>
             <input type='text' name="firstname" className='form-control mb-3' placeholder='First name'
-            {...register('firstname',{required:true,minLength:4,maxLength:6})}/>
+            {...register('firstName',{required:true,minLength:4,maxLength:6})}/>
             
             {errors.firstname?.type==='required' && <p  className='text-danger-lead fs-5'>Firstname is required</p>}
             {errors.firstname?.type==='minLength' && <p  className='text-danger-lead fs-5'>minLength is 4</p>}
@@ -36,7 +36,7 @@ export default function CampaignEnrollmentForm() {
             <div className='col-xs-1 col-6 mb-2'>
             <label htmlFor="lastname" className='mb-2 mx-2 form-label'>LastName</label>
             <input type='text' name="lastname" className='form-control mb-2' placeholder='Last name'
-            {...register('lastname',{required:true,minLength:4,maxLength:6})}/>
+            {...register('lastName',{required:true,minLength:4,maxLength:6})}/>
             
             {errors.lastname?.type==='required' && <p  className='text-danger-lead fs-5'>lastname is required</p>}
             {errors.lastname?.type==='minLength' && <p  className='text-danger-lead fs-5'>minLength is 4</p>}
@@ -46,7 +46,7 @@ export default function CampaignEnrollmentForm() {
             </div>
             <div>
             <label htmlFor="age" className='mb-2 mx-2 form-label'>Age</label>
-            <input type="number" placeholder="Age" {...register("Age", {required: true, max: 3})} />
+            <input type="number" placeholder="Age" {...register("age", {required: true, max: 3})} />
             </div>
             <div>
               <h6>Select Gender</h6>
@@ -60,20 +60,20 @@ export default function CampaignEnrollmentForm() {
                 </div>
             </div>
             <div>
-            <label htmlFor="age" className='mb-2 mx-2 form-label'>Mobile Number</label>
+            <label htmlFor="mobileno" className='mb-2 mx-2 form-label'>Mobile Number</label>
 
-            <input type="tel" placeholder="Mobile number" {...register("Mobile number", {required: true})} />
-
-        </div>
-        <div>
-            <label htmlFor="age" className='mb-2 mx-2 form-label'>Email</label>
-
-            <input type="text" placeholder="Email" {...register("Email", {required: true, pattern: /^\S+@\S+$/i})} />
+            <input type="tel" placeholder="Mobile number" {...register("mobileNumber", {required: true})} />
 
         </div>
         <div>
-            <label htmlFor="age" className='mb-2 mx-2 form-label'>Blood Group</label>
-            <select {...register("Blood Group", { required: true })}>
+            <label htmlFor="email" className='mb-2 mx-2 form-label'>Email</label>
+
+            <input type="text" placeholder="Email" {...register("email", {required: true, pattern: /^\S+@\S+$/i})} />
+
+        </div>
+        <div>
+            <label htmlFor="bloodGroup" className='mb-2 mx-2 form-label'>BloodGroup</label>
+            <select {...register("bloodGroup", { required: true })}>
         <option value="O+">O+</option>
         <option value="O-">O-</option>
         <option value="A+">A+</option>
@@ -85,32 +85,32 @@ export default function CampaignEnrollmentForm() {
       </select>
       </div>
         <div>
-            <label htmlFor="age" className='mb-2 mx-2 form-label'>Hospital Name</label>
+            <label htmlFor="hospitalName" className='mb-2 mx-2 form-label'>HospitalName</label>
 
-            <input type="text" placeholder="Hospital Name" {...register("Hospital Name", {required: true, maxLength: 78})} />
+            <input type="text" placeholder="Hospital Name" {...register("hospitalName", {required: true, maxLength: 78})} />
 
         </div>
         <div>
-            <label htmlFor="age" className='mb-2 mx-2 form-label'>Hospital Address</label>
+            <label htmlFor="age" className='mb-2 mx-2 form-label'>HospitalAddress</label>
 
             <textarea rows='5' className='form-control mb-2' {...register('address')}>Address</textarea>
 
         </div>
         <div>
-        <label htmlFor="age" className='mb-2 mx-2 form-label'>Patient RoomNo</label>
-        <input type="text" placeholder="Patient Room Number" {...register("Patient Room Number", {})} />
+        <label htmlFor="age" className='mb-2 mx-2 form-label'>Patient Room No</label>
+        <input type="text" placeholder="Patient Room Number" {...register("patientRoomNo", {})} />
 
         </div>
         <div>
             <label htmlFor="age" className='mb-2 mx-2 form-label'>Fund Required</label>
 
-            <input type="number" placeholder="Fund Required" {...register("Fund Required", {required: true})} />
+            <input type="number" placeholder="Fund Required" {...register("fundRequired", {required: true})} />
 
         </div>
         <div>
             <label htmlFor="age" className='mb-2 mx-2 form-label'>Funding Deadline</label>
 
-            <input type="datetime" placeholder="Deadline" {...register("Deadline for Collecting Funds", {required: true})} />
+            <input type="datetime" placeholder="Deadline" {...register("fundingDeadline", {required: true})} />
 
         </div>
    
