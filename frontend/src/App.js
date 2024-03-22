06fba9dcb8c3a2147d8acb36fa32b36ac4a32a1e
Navaneeth Reddy Nalla <navaneethreddynalla@gmail.com>
2024-03-22 20:30:28 +0530

    Removed unsused Navigate import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,32 @@
-import './App.css';
-import { createBrowserRouter,Navigate,RouterProvider } from 'react-router-dom';
-import RootLayout from './Components/RootLayout/RootLayout'
-import Homepage from './Components/HomePage/HomePage'
-import CampaignEnrollment from './Components/Enrollment/CampaignEnrollment/CampaignEnrollmentForm'
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RootLayout from "./Components/RootLayout/RootLayout";
+import Homepage from "./Components/HomePage/HomePage";
+import CampaignEnrollment from "./Components/Enrollment/CampaignEnrollment/CampaignEnrollmentForm";
 
 // import CampaignEnrollmentForm from './Components/CampaignEnrollmentForm';
 
 function App() {
-
   let browserRouter = createBrowserRouter([
     {
-      path: '',
-      element:<RootLayout/>,
-      children:[
+      path: "",
+      element: <RootLayout />,
+      children: [
         {
-          path: '',
-          element:<Homepage/>
+          path: "",
+          element: <Homepage />,
         },
         {
-          path: 'create-campaign',
-          element: <CampaignEnrollment/>
-        }
-      ]
-    }
-  ])
- 
+          path: "create-campaign",
+          element: <CampaignEnrollment />,
+        },
+      ],
+    },
+  ]);
+
   return (
     <div className="App">
-      <RouterProvider router={browserRouter}/>
+      <RouterProvider router={browserRouter} />
     </div>
   );
 }
