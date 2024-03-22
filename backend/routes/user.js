8e88c78034864e209499ef487f73fa7deee4432e
Navaneeth Reddy Nalla <navaneethreddynalla@gmail.com>
2024-03-22 16:03:11 +0530

    Added User Login backend

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const expressAsyncHandler = require("express-async-handler");
 const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const userRouter = express.Router();
 
 let userCollection, campaignCollection;
@@ -11,11 +12,10 @@ userRouter.use((req, res, next) => {
   next();
 });
 
+// User Creation
 userRouter.post(
   "/new-user",
   expressAsyncHandler(async (req, res) => {
-    userCollection = req.app.get("userCollection");
-
     const user = req.body;
     const dbUser = await userCollection.findOne({ username: user.username });
 
@@ -28,6 +28,39 @@ userRouter.post(
 
     await userCollection.insertOne(user);
     res.send({ message: "User Created", statusCode: 2 });
+  })
+);
+
+// User Login
+userRouter.post(
+  "/login",
+  expressAsyncHandler(async (req, res) => {
+    const user = req.body;
+    const dbUser = await userCollection.findOne({ username: user.username });
+
+    if (dbUser === null) {
+      return res.send({ statusCode: 3, message: "Invalid Username" });
+    }
+
+    const status = await bcryptjs.compare(user.password, dbUser.password);
+
+    if (status === false) {
+      return res.send({ message: "Invalid Password", status: 3 });
+    }
+
+    const token = jwt.sign(
+      { username: user.username },
+      process.env.SECRET_KEY,
+      { expiresIn: "1d" }
+    );
+
+    delete dbUser.password;
+    res.send({
+      message: "Login Success",
+      statusCode: 4,
+      token: token,
+      user: dbUser,
+    });
   })
 );
 
