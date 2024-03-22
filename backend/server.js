08b0463a7734e9df24f39f6a994c6b6d03d5d2ec
Navaneeth Reddy Nalla <navaneethreddynalla@gmail.com>
2024-03-22 15:00:06 +0530

    Established DB connection

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,22 @@
 const express = require("express");
 const app = express();
+const mongoClient = require("mongodb").MongoClient;
 require("dotenv").config();
+
+mongoClient
+  .connect(process.env.DB_URL)
+  .then((client) => {
+    const dbObj = client.db(process.env.DB_NAME);
+
+    const campaignCollection = dbObj.collection("campaignsCollection");
+    const userCollection = dbObj.collection("usersCollection");
+
+    app.set("userCollection", userCollection);
+    app.set("campaignCollection", campaignCollection);
+
+    console.log("Established Connection with the Database.");
+  })
+  .catch((error) => console.log(error));
 
 const port = process.env.PORT || 4040;
 
